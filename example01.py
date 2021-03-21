1f67fccb63e622ef8fb7e106da979ac942c973fc
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-21 13:57:29 +0000

    Update example01.py

--- example01.py
+++ example01.py
@@ -1,9 +1,11 @@
 import os
 import requests
 import update
-
+import sys
 
 link = "https://raw.githubusercontent.com/CoalByte/dummy/main/example01.py"
 
-
-update.main(url=link)
+if sys.argv:
+    update.main(url=link)
+elif sys.argv[0] == "upd-done":
+    print("Skipping update (auto restart previously executed)")
