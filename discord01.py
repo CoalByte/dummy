732390bcf09751b9f2c7da3bf7f034ffefbed838
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-22 09:38:59 +0000

    Update discord01.py

--- discord01.py
+++ discord01.py
@@ -4,7 +4,7 @@ from update import Loop
 __version__ = 1
 
 TOKEN = open("token.txt", "r").read()
-link = "https://raw.githubusercontent.com/CoalByte/dummy/main/example01.py"
+link = "https://raw.githubusercontent.com/CoalByte/dummy/main/discord01.py"
 update_loop = Loop(seconds=10, func=update.main, arguments=link)
 bot = commands.Bot(command_prefix="!!")
 
