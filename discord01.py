070eb6b27954461fb18fe4549e3acb6cbe546388
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-22 09:38:45 +0000

    Create discord01.py

---
+++ discord01.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+import update
+from update import Loop
+__version__ = 1
+
+TOKEN = open("token.txt", "r").read()
+link = "https://raw.githubusercontent.com/CoalByte/dummy/main/example01.py"
+update_loop = Loop(seconds=10, func=update.main, arguments=link)
+bot = commands.Bot(command_prefix="!!")
+
+
+print(f"New session started.... (v{__version__})")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, World!")
+
+
+update_loop.start()
+bot.run(TOKEN)
