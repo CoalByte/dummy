be54030fecad2cbd7531d49a0331dabe2548f533
Kendohu <62639622+CoalByte@users.noreply.github.com>
2021-03-22 09:51:48 +0000

    Update discord01.py

--- discord01.py
+++ discord01.py
@@ -1,14 +1,21 @@
 from discord.ext import commands
 import update
 from update import Loop
-__version__ = 3
+import asyncio
+__version__ = 4
 
 TOKEN = open("token.txt", "r").read()
 link = "https://raw.githubusercontent.com/CoalByte/dummy/main/discord01.py"
 
 
+def shutdown():
+    async def internal():
+        await bot.logout()
+    asyncio.run(internal())
+
+
 def upd(arg):
-    update.main(source=arg, filename="bot.py", pre_boot=bot.logout)
+    update.main(source=arg, filename="bot.py", pre_boot=shutdown)
     
     
 update_loop = Loop(seconds=10, func=upd, arguments=link)
@@ -20,7 +27,7 @@ print(f"New session started.... (v{__version__})")
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Hello, World! I updated this again!")
+    await ctx.send(f"Hello, World! I updated this again! {str(__version__)}")
 
 
 update_loop.start()
